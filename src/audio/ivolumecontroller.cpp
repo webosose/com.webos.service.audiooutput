185bd0cc2a5de15e24c4ca48b10d53bc59ac5437
Balamurugan Krishnan <balamurugan.krishnan@lge.com>
2020-10-01 12:34:50 +0000

    Fix static issue in audiooutputd
    :Release Notes:
    Fix static issue related to unsigned comparison in audiooutputd
    
    :Detailed Notes:
    Addressed below static issue
    CID-113927 Unsigned compared against 0
    
    :Testing Performed:
    Tested on RPI4 rebased on OSE build #251
    Test cases: QWA-3810 QWA-5429
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [CID-113927] Unsigned compared against 0
    
    Change-Id: I81d813cbd6a8e3ade65210b3e425b517472a9e73

--- src/audio/ivolumecontroller.cpp
+++ src/audio/ivolumecontroller.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 LG Electronics, Inc.
+// Copyright (c) 2018-2020 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,20 +19,19 @@
 
 bool IVolumeController::setVolume(SpeakerVolume newVolume)
 {
-    if (newVolume > MAX_VOLUME || newVolume < MIN_VOLUME)
+    if (newVolume <= MAX_VOLUME && newVolume >= MIN_VOLUME)
     {
+        SpeakerVolume oldVolume = mVolume;
+        mVolume = newVolume;
+        bool success = onVolumeChanged();
+        if (!success)
+        {
+            mVolume = oldVolume;
+        }
+        return success;
+    }
+    else
         return false;
-    }
-
-    SpeakerVolume oldVolume = mVolume;
-    mVolume = newVolume;
-    bool success = onVolumeChanged();
-
-    if (!success)
-    {
-        mVolume = oldVolume;
-    }
-    return success;
 };
 
 bool IVolumeController::setMute(bool muteFlag)
